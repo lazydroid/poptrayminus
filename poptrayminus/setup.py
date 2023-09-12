d110511e713d36293f0b442144e7c08afaa124e6
lenik terenin <lenik@lazydroid.com>
2023-09-13 01:35:53 +0900

    first public version 0.5.0

---
+++ poptrayminus/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = 'poptrayminus',
+	version = '0.5.0',
+	description = 'POP3 mailbox monitor with tray icon notifications',
+	long_description = 'PopTray Minus checks POP3 mailbox, filters spam and shows message header previews.',
+	author = 'Lenik Terenin',
+	author_email = 'lenik@lazydroid.com',
+	license = "GPL",
+	url = 'http://lazydroid.com/poptrayminus/',
+	data_files = [
+		('share/poptrayminus', [
+			'poptrayminus.png'
+			]),
+		('share/pixmaps', ['poptrayminus.png']),
+		('share/applications', ['poptrayminus.desktop']),
+	],
+	scripts = ["poptrayminus"],
+	keywords = "pop3 linux ubuntu mailbox spam tray",
+)
